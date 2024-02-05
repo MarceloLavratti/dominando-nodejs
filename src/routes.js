ea975bc71ef8f3e673e3e3d4da439b72fe3d2b15
MarceloLavratti <marcelolavratti@gmail.com>
2024-02-05 17:26:31 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+import customers from "./app/controllers/CustomersController";
+
+const routes = new Router();
+
+routes.get("/customers", customers.index);
+routes.get("/customers/:id", customers.show);
+routes.post("/customers/:id", customers.create);
+routes.put("/customers/:id", customers.update);
+routes.delete("/customers/:id", customers.destroy);
+
+
+export default routes;
